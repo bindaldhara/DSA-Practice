a5e85fd966b4d1863e999f27754a0b08e111d885
Dhara Bindal <84957152+bindaldhara@users.noreply.github.com>
2022-01-08 00:25:21 +0530

    Time: 12 ms (14.15%), Space: 13.7 MB (46.52%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    vector<string>res;
+public:
+    vector<string> generateParenthesis(int n) {
+        if(!n)
+            return {};
+        int open = n, close = 0;
+        Paranthesis("", open, close);
+        return res;
+    }
+    void Paranthesis(string curr, int open, int close){
+        if(open == 0 and close == 0){
+            res.push_back(curr);
+            return;
+        }
+        if(open > 0){
+            Paranthesis(curr+'(', open-1, close+1); 
+        }
+        if(close > 0){
+            Paranthesis(curr+')', open, close-1);
+        }
+    }
+};

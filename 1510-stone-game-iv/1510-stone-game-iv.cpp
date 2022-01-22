b1c93eb0362c321c9514f59d99bf34a79c372fe1
Dhara Bindal <84957152+bindaldhara@users.noreply.github.com>
2022-01-22 11:12:53 +0530

    Time: 59 ms (58.39%), Space: 6.2 MB (89.60%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<bool> dp(n+1); 
+        for (int i = 1; i <= n; i++) 
+            for (int k = 1; k*k <= i;k++) 
+                if (!dp[i-k*k]) {
+                    dp[i] = true; 
+                    break; 
+                }
+        return dp.back(); 
+    }
+};

07db28e2724d901fc7678739a0fccb60e6bbab8b
Dhara Bindal <84957152+bindaldhara@users.noreply.github.com>
2022-02-02 12:38:21 +0530

    Time: 68 ms (16.77%), Space: 8.5 MB (99.07%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+     bool same(char a1[],char a2[])
+    {
+        for(int i=0;i<256;i++)
+        {
+            if(a1[i]!=a2[i])
+                return false;
+        }
+        return true;
+    }
+    
+    vector<int> findAnagrams(string s, string p) {
+        int n = p.length();
+        if(s.length()<n)
+        {
+            vector<int> a;
+            return a;
+        }
+        char pat[256]={0};
+        char txt[256]={0};
+        vector<int> ans;
+        
+        for(int i=0;i<n;i++)
+        {
+            pat[p[i]]++;
+            txt[s[i]]++;
+        }
+        
+        for(int i=n;i<s.length();i++)
+        {
+            if(same(pat,txt))
+            {
+                int ind = i-n;
+                ans.push_back(ind);
+            }
+            
+            txt[s[i]]++;
+            txt[s[i-n]]--;
+        }
+        
+        if(same(pat,txt))
+        {
+            int ind = s.length()-n;
+            ans.push_back(ind);
+        }
+        
+        return ans;
+        
+    }
+};

f413a2a786e511d1718b6ef97c2c3b7a0ee119a8
Dhara Bindal <84957152+bindaldhara@users.noreply.github.com>
2022-03-22 23:32:19 +0530

    Time: 85 ms (5.27%), Space: 27.6 MB (13.52%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int sz = nums.size();
+        int low = 0;
+        int high = sz-1;
+        while (low <= high) {
+            int mid = (low+high)/2;
+            if (nums[mid] < target)
+                low = mid+1;
+            else if (nums[mid] > target)
+                high = mid-1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+};

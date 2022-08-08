e4bc0fdeed00ab9c64fda7a4fdc95a7175f5f5c9
Dhara Bindal <84957152+bindaldhara@users.noreply.github.com>
2022-08-08 23:22:10 +0530

    Time: 344 ms (85.71%), Space: 10.2 MB (100.00%) - LeetHub

---
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int longestIdealString(string s, int k) {
+      vector<int> dp(150);
+        int ans = 0;
+        for (auto &i : s){
+            for (int j = i - k; j <= i + k; j++)
+                dp[i] = max(dp[i], dp[j]);
+            ans = max(ans, ++dp[i]);
+        }
+        return ans;  
+    }
+};
+

784f3f9023674e319108ea0f97503f78c16d682e
Dhara Bindal <84957152+bindaldhara@users.noreply.github.com>
2022-09-07 09:07:50 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (27.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
+    int helper(int i,int j,vector<vector<int>>&dp,int m,int n){
+        if(i>=m || j>=n)
+            return 0;
+        if(i==m-1 || j==n-1)
+            return 1;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        return dp[i][j]=helper(i+1,j,dp,m,n)+helper(i,j+1,dp,m,n);
+    }
+    
     int uniquePaths(int m, int n) {
-      int dp[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 || j==0)
-                    dp[i][j]=1;
-                else
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
+     vector<vector<int>> dp(m,vector<int>(n,-1));
+        return helper(0,0,dp,m,n);
     }
 };
 

ccad305e1cdc0dfeb8cac9ac2dfb4a85407bfaad
Dhara Bindal <84957152+bindaldhara@users.noreply.github.com>
2022-09-17 14:14:51 +0530

    Time: 25 ms (27.64%), Space: 21.7 MB (30.63%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -15,7 +15,6 @@ bool isValidBST(TreeNode* root) {
 
         vector<int> res;
         inOrderTrav(root,res);
-    
         for (int i = 1; i < res.size(); i++){
             if (res[i] <= res[i-1])
                 return false;
@@ -26,7 +25,6 @@ bool isValidBST(TreeNode* root) {
     void inOrderTrav(TreeNode* root, vector<int>& res){
         if (!root)
             return;
-        
         inOrderTrav(root->left, res);
         res.push_back(root->val);
         inOrderTrav(root->right, res);

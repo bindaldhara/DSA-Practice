3bdb3401df81b9bf7d0b01d9945687c169f6a111
Dhara Bindal <84957152+bindaldhara@users.noreply.github.com>
2022-09-22 10:35:20 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (28.29%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     bool backspaceCompare(string s, string t) {
           stack<char>s1,s2;
-           for(auto i:s)
-           { 
+           for(auto i:s){ 
                 if(i>='a'&&i<='z')s1.push(i);
               if(i=='#'&&s1.size()>0)s1.pop();
-             
            }
-           for(auto i:t)
-           { 
+           for(auto i:t){ 
             if(i>='a'&&i<='z')s2.push(i);
             if(i=='#'&&s2.size()>0)s2.pop();
            }

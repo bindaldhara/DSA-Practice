0c0d7d1971fa7cfe582bd073cf75894ce6c39320
Dhara Bindal <84957152+bindaldhara@users.noreply.github.com>
2023-01-24 22:49:31 +0530

    Time: 70 ms (81.13%), Space: 20.1 MB (72.10%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        if(grid[0][0]==1 || grid[grid.size()-1][grid.size()-1]==1)
+        {
+            return -1;
+        }
+        queue<pair<pair<int, int>, int>> q;
+        q.push({{0, 0}, 1});
+        while(!q.empty()){
+            auto it=q.front();
+            int row=it.first.first;
+            int col=it.first.second;
+            int dis=it.second;
+            q.pop();
+            if(row==grid.size()-1 && col==grid.size()-1) 
+                return dis;
+            if(row-1>=0 && grid[row-1][col]!=1)
+            {
+                q.push({{row-1, col}, dis+1});
+                grid[row-1][col]=1;
+            }
+            
+            if(row+1<grid.size() && grid[row+1][col]!=1)
+            {
+                q.push({{row+1, col}, dis+1});
+                grid[row+1][col]=1;
+            }
+    
+            if(col-1>=0 && grid[row][col-1]!=1)
+            {
+                q.push({{row, col-1}, dis+1});
+                grid[row][col-1]=1;
+            }
+           
+            if(col+1<grid.size() && grid[row][col+1]!=1)
+            {
+                q.push({{row, col+1}, dis+1});
+                grid[row][col+1]=1;
+            }
+          
+            if(row+1<grid.size() && col-1>=0 && grid[row+1][col-1]!=1)
+            {
+                q.push({{row+1, col-1}, dis+1});
+                grid[row+1][col-1]=1;
+            }
+          
+            if(row+1<grid.size() && col+1<grid.size() && grid[row+1][col+1]!=1)
+            {
+                q.push({{row+1, col+1}, dis+1});
+                grid[row+1][col+1]=1;
+            }
+            
+            if(row-1>=0 && col-1>=0 && grid[row-1][col-1]!=1)
+            {
+                q.push({{row-1, col-1}, dis+1});
+                grid[row-1][col-1]=1;
+            }
+           
+            if(row-1>=0 && col+1<grid.size() && grid[row-1][col+1]!=1)
+            {
+                q.push({{row-1, col+1}, dis+1});
+                grid[row-1][col+1]=1;
+            }
+        }
+        return -1;
+    }
+};

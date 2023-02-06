56d35f37c287e9f01b9ccee094414206ff099658
Dhara Bindal <84957152+bindaldhara@users.noreply.github.com>
2023-02-06 19:20:29 +0530

    Time: 18 ms (83.47%), Space: 16.9 MB (55.70%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-      int n=nums.size();
-      unordered_map<int,int> mp(n);
-  
-      for(int i=0;i<n;i++)
-      mp[nums[i]]++;
-      for(int i=0;i<n;i++){
-        if(mp[nums[i]]==1)
-          return nums[i];
-      }
-      return 0;  
+       int n = nums.size(); 
+        for(int i = 0; i < n - 1; i++){
+            nums[i + 1] = nums[i] ^ nums[i + 1];
+        }
+        return nums[n- 1]; 
     }
 };

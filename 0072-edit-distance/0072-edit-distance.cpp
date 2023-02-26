f524a559ef89276c9dfeae7e7b3dc2a6a2538d75
Dhara Bindal <84957152+bindaldhara@users.noreply.github.com>
2023-02-26 11:42:02 +0530

    Time: 8 ms (87.54%), Space: 9.1 MB (42.85%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
-    int f(int i, int j, string &s, string &t, vector<vector<int>>&dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
+    int helper(int i, int j, string &s, string &t, vector<vector<int>>&dp){
+        if(i<0) 
+            return j+1;
+        if(j<0)
+            return i+1;
         int res=0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]) res=f(i-1,j-1,s,t,dp);
+        if(dp[i][j]!=-1) 
+            return dp[i][j];
+        if(s[i]==t[j]) 
+            res=helper(i-1,j-1,s,t,dp);
         else{
-            res=1+min(f(i-1,j-1,s,t,dp),min(f(i-1,j,s,t,dp),f(i,j-1,s,t,dp)));
+            res=1+min(helper(i-1,j-1,s,t,dp),min(helper(i-1,j,s,t,dp),helper(i,j-1,s,t,dp)));
         }
         return dp[i][j]=res;
     }
@@ -15,7 +19,7 @@ public:
         int l1=word1.size();
         int l2=word2.size();
         vector<vector<int>>dp(l1+1,vector<int>(l2+1,-1));
-        return f(l1-1,l2-1,word1,word2,dp);
-        
+        return helper(l1-1,l2-1,word1,word2,dp);
     }
 };
+

fb83c53ce04699dbc30104d49192270a05833087
Dhara Bindal <84957152+bindaldhara@users.noreply.github.com>
2023-03-06 11:21:55 +0530

    Time: 36 ms (32.41%), Space: 10.8 MB (27.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,14 +6,12 @@ public:
 
         int i = 0, j = 0;
         int count = 0;
-    while (j < n) {
-        if (mp.find(s[j]) == mp.end())
-        {
+     while (j < n) {
+        if (mp.find(s[j]) == mp.end()){
             mp.insert(s[j]);
             ++j;
         }
-        else
-        {
+        else{
             mp.erase(s[i]);
             ++i;
         }
